34f0882ed9f5be319b78286452c6f23ec6b1a181
Decho Kocharin <sourcedecho@outlook.com>
2018-11-28 23:39:21 +0700

    Stratum: Add Binarium-V1 Algo

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -221,3 +221,4 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/argon2d.h"
 #include "algos/pipehash.h"
 #include "algos/dedal.h"
+#include "algos/binarium_hash_v1.h"
