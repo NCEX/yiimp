ebeb30ac86f7e5ed6b9f1cf956ce0d91712ad957
Decho Kocharin <sourcedecho@outlook.com>
2018-11-30 09:45:43 +0700

    Stratum: Add X21S Algorithm

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -222,3 +222,4 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/pipehash.h"
 #include "algos/dedal.h"
 #include "algos/binarium_hash_v1.h"
+#include "algos/x21s.h"
