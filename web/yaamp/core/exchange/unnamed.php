c636369249b49af84bb71aae2dc5b993d9513717
Decho Kocharin <sourcedecho@outlook.com>
2020-04-14 12:56:09 +0700

    Update unnamed.php

--- web/yaamp/core/exchange/unnamed.php
+++ web/yaamp/core/exchange/unnamed.php
@@ -1,14 +1,9 @@
 <?php
-
-// https://api.unnamed.exchange/v1/
-
-
+	//https://api.unnamed.exchange/v1/
 function unnamed_api_query($method, $params='')
 {
 	$uri = "https://api.unnamed.exchange/v1/Public/$method";
-	if (!empty($params))
-		$uri .= "$params";
-
+	
 	$ch = curl_init($uri);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
@@ -18,7 +13,7 @@ function unnamed_api_query($method, $params='')
 	$result = json_decode($res);
 	if(!is_object($result) && !is_array($result)) {
 		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		debuglog("altmarkets: $method failed ($status) ".strip_data($res));
+		debuglog("unnamed: $method failed ($status) ".strip_data($res));
 	}
 
 	curl_close($ch);
