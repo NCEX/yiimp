0eab1fdc9b9ec93c28f6a13437c93afd23a73c51
Decho Kocharin <sourcedecho@outlook.com>
2020-06-01 00:37:06 +0700

    Update block_results.php

--- web/yaamp/modules/site/block_results.php
+++ web/yaamp/modules/site/block_results.php
@@ -98,7 +98,7 @@ foreach($db_blocks as $db_block)
 			$t = (int) ($coin->mature_blocks - $db_block->confirmations) * $coin->block_time;
 			$eta = "ETA: ".sprintf('%dh %02dmn', ($t/3600), ($t/60)%60);
 		}
-		echo "<span title=\"$eta\">Immature ({$db_block->confirmations})</span>";
+		echo "<span title=\"$eta\">Immature ({$db_block->confirmations}/{$coin->mature_blocks})</span>";
 	}
 
 	else if($db_block->category == 'generate')
