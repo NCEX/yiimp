2cdf6b554cbf317b217ca11d1e15dca61f8a84cd
NCEX <73142334+NCEX@users.noreply.github.com>
2020-11-01 20:28:33 +0100

    Update main.php

--- web/yaamp/ui/main.php
+++ web/yaamp/ui/main.php
@@ -142,7 +142,7 @@ function showPageFooter()
 	$year = date("Y", time());
 
 	echo "<p>&copy; $year ".YAAMP_SITE_NAME.' - '.
-		'<a href="https://github.com/Kudaraidee/yiimp">Open source Project</a></p>';
+		'<a href="https://github.com/NCEX/yiimp">Open source Project</a></p>';
 
 	echo '</div><!-- footer -->';
 }
